f4c6d0ab22d3bcb56305a21d5bd052ee42d85da7
binpeng zhang <binpeng.d.zhang@gmail.com>
2019-01-14 23:39:29 -0500

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID("5c342c8f6cca6316d114fa20")
+  // },{
+  //   $set: {
+  //     completed: true
+  //   }
+  // },{
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID("5c342cbcf08a5416d32bbcf3")
+  },{
+    $set: {
+      name: "SupaCool"
+    },
+    $inc: {
+      age: 1
+    }
+  },{
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
