c14c8c5b474a5a1a74b55087f79394cea05cb968
binpeng zhang <binpeng.d.zhang@gmail.com>
2019-02-01 16:20:37 -0500

    add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -3,16 +3,17 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/user')
+var {User} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -25,19 +26,15 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
-
-app.listen(3000, () => {
-  console.log('Started on port 3000');
-});
-
 
 module.exports = {app};
+
 
 // var newTodo = new Todo({
 //   text: 'cook dinner'
